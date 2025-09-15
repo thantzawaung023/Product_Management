64e07ac7c64c6b59f3c3fcc8a556d541560c4027
mtmthantzawaung <thantzawaung@metateammyanmar.com>
2025-09-15 09:55:36 +0630

    fix : ci fail

--- lib/utils/storage/language_setting.dart
+++ lib/utils/storage/language_setting.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get_storage/get_storage.dart';
-import 'package:product_management/config/config.dart';
 
 class CurrentLanguageSetting {
   factory CurrentLanguageSetting() {
